51259f98735c139d453cce43f0fb7de2242fd0d0
quang.nguyen <quang.nguyen@gremsy.com>
2025-10-27 17:19:03 +0700

    add mobile model

--- realesrgan/archs/srvgg_arch.py
+++ realesrgan/archs/srvgg_arch.py
@@ -136,7 +136,7 @@ class SRVGGNetCompactInfer(nn.Module):
 
         out = self.upsampler(out)
         # add the nearest upsampled image, so that the network learns the residual
-        base = F.interpolate(x, scale_factor=self.upscale, mode='bilinear')
-        out += base
+        # base = F.interpolate(x, scale_factor=self.upscale, mode='bilinear',align_corners=False)
+        # out += base
         out = torch.clamp(out, 0.0, 1.0)
         return out
